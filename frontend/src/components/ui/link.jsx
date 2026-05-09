f68f0366941e73c10658b7cf8262c97b445ad0b7
gen-hiroto0119 <hiro_genfuru0119@icloud.com>
2026-05-09 15:58:55 +0900

    fix: remove next/link dependency for Vite compatibility

--- frontend/src/components/ui/link.jsx
+++ frontend/src/components/ui/link.jsx
@@ -1,11 +1,10 @@
 import * as Headless from '@headlessui/react'
-import NextLink from 'next/link'
 import { forwardRef } from 'react'
 
 export const Link = forwardRef(function Link(props, ref) {
   return (
     <Headless.DataInteractive>
-      <NextLink {...props} ref={ref} />
+      <a {...props} ref={ref} />
     </Headless.DataInteractive>
   )
 })
